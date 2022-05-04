080496873f0368388532d83ab33df4347dd51ee0
abg5043 <aaron.b.grant@gmail.com>
2022-05-04 10:27:58 -0500

    Fixed documentation issues.

--- src/main/java/org/example/fightclubgame/App.java
+++ src/main/java/org/example/fightclubgame/App.java
@@ -17,7 +17,7 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 
 /**
- * Driver for JavaFX version of fight game
+ * Driver for JavaFX version of a fighting game
  *
  * @author Aaron Grant
  * @since May 2022
@@ -31,65 +31,75 @@ public class App extends Application {
   public static VBox currentRosterBox = new VBox();
 
 
+  //start method is what initiates JavaFX; this encapsulates the setting up of JavaFX components
   @Override
   public void start(Stage stage) {
     stage.setTitle("Battle for Tar Valon");
     String s = "Welcome to the Battle Royale!";
 
+    //set up the center of the borderpane
     addLabelToCenter(s);
-
     ScrollPane scrollPane = new ScrollPane(center);
     borderPane.setCenter(scrollPane);
 
+    //set up the bottom of border pane as fight announcement
     setFightAnnouncement(new TextField("No fights to announce. Please stand by."));
     fightAnnouncement.setAlignment(Pos.CENTER);
     fightAnnouncement.setBackground(new Background(new BackgroundFill(Color.GOLDENROD, null, null)));
-    fightAnnouncement.setStyle("-fx-text-fill: grey; -fx-font-size: 16px;");
+    fightAnnouncement.setStyle("-fx-text-fill: white; -fx-font-size: 16px;");
     borderPane.setBottom(fightAnnouncement);
 
+    //set up the top of the border pane as a round announcement
     setRoundAnnouncement(new TextField("No rounds to announce. Please stand by."));
     roundAnnouncement.setAlignment(Pos.CENTER);
     roundAnnouncement.setBackground(new Background(new BackgroundFill(Color.GOLDENROD, null, null)));
-    roundAnnouncement.setStyle("-fx-text-fill: grey; -fx-font-size: 16px;");
+    roundAnnouncement.setStyle("-fx-text-fill: white; -fx-font-size: 16px;");
     borderPane.setTop(roundAnnouncement);
 
-
-    overallRosterBox.setMinWidth(250);
-    borderPane.setRight(overallRosterBox);
-
+    //set up the right of the border pane as a log of all current monsters in the round
     currentRosterBox.setMinWidth(250);
-    borderPane.setLeft(currentRosterBox);
+    borderPane.setRight(currentRosterBox);
 
+    //set up the left of the border pane as a log of all monsters who are competing in the tournament
+    overallRosterBox.setMinWidth(250);
+    borderPane.setLeft(overallRosterBox);
+
+    //set the dimensions of the screen and start the game
     var scene = new Scene(borderPane, 1024, 768);
     stage.setScene(scene);
     stage.show();
-
     mainApp();
   }
 
+  //main method launches start method
   public static void main(String[] args) {
     launch();
   }
 
+  //method for adding labels to the center pain by passing in a string array
   public static void addLabelToCenter(String[] messages) {
     for (String message : messages) {
       addLabelToCenter(message);
     }
   }
 
+  //method for adding labels to the center pain by passing in an ArrayList
   public static void addLabelToCenter(ArrayList<String> messages) {
     for (String message : messages ) {
       addLabelToCenter(message);
     }
   }
 
+  //method for adding labels to the center pain by passing in a single string
   public static void addLabelToCenter(String message) {
     center.getChildren().add(new Label("    " + message));
   }
 
+  //main driver for the app. This encapsulates all game logic
   public static void mainApp() {
-    ArrayList<Player> roster = new ArrayList<>();
 
+    //create a roster and add players to it
+    ArrayList<Player> roster = new ArrayList<>();
     Player p = new Player ("Ptomain Tony", new Cook());
     Player q = new Player("Broccoli Spears", new Cook());
     Player r = new Player("Honey Boo Boo", new Badger());
@@ -105,17 +115,26 @@ public class App extends Application {
     roster.add(new Player("Sunny", new Sunflower()));
     roster.add(new Player("Cher", new Sunflower()));
 
-    FxGameManager gm = new FxGameManager();
-
+    //adds the overall roster to the overallRosterBox
     displayOverallRoster(roster);
 
+    //initiate a new GameManager
+    FxGameManager gm = new FxGameManager();
+
+    //starts the contest with the game manager
     gm.contest(roster);
   }
 
+  //adds a blank line to the center pane
   public static void blankLine() {
     addLabelToCenter(" ");
   }
 
+  /**
+   * this adds and stylizes a roster to the overallRosterBox
+   *
+   * @param roster - an arraylist of players
+   */
   private static void displayOverallRoster(ArrayList<Player> roster) {
     overallRosterBox.getChildren().clear();
     blankLine();
--- src/main/java/org/example/fightclubgame/FxGameManager.java
+++ src/main/java/org/example/fightclubgame/FxGameManager.java
@@ -9,32 +9,47 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Color;
 
+/**
+ * A class that manages the game. This extends GameManager from the command-line version of this app
+ * so that it can function within a GUI
+ */
 public class FxGameManager extends GameManager {
-  private Boolean firstTime = true;
+  private Boolean firstTimeRoundAnnouncement = true; //keeps track of if displayMessage had found a round announcement yet
 
+  /**
+   * A method that handles displaying messages within the GUI app
+   *
+   * @param message - a string message to be displayed
+   */
   @Override
   public String displayMessage(String message) {
-
-    System.out.println(message);
-
-
+    //determine if this is a round announcement or not
     if(message.contains("Maximum remaining rounds")) {
-      if(!firstTime) {
+      //first time announcmeents should not have blank lines added
+      if(!firstTimeRoundAnnouncement) {
         App.blankLine();
         App.blankLine();
         App.blankLine();
         App.blankLine();
       } else {
-        firstTime = false;
+        firstTimeRoundAnnouncement = false;
       }
+
+      //if it's a round announcement, break out the info
       String[] roundInfo = message.split("═════════");
+      //first fine the round title
       String roundTitle = roundInfo[1];
+      //then the round number
       String roundNumber = roundTitle.substring(7,8);
+      //finally, make a string array of current roster
       String[] currentRoster = roundInfo[2].substring(4).split("  ");
 
+      //add this information to the display
       displayCurrentRoster(currentRoster, roundNumber);
-      displayRoundAnnouncement(message.split("═════════")[1]);
-    }else if(message.contains("WINS!")) {
+      displayRoundAnnouncement(roundTitle);
+
+    } else if(message.contains("WINS!")) {
+      //if this is a win announcement, create an alert after adding to the center
       App.addLabelToCenter(message);
 
       Alert a = new Alert(Alert.AlertType.INFORMATION);
@@ -43,6 +58,7 @@ public class FxGameManager extends GameManager {
       a.setContentText(message);
       a.show();
     } else {
+      //just add info to the center if it's anything else, but if there was a line separator, make sure to add that too
       App.addLabelToCenter(message);
       if(message.contains(System.getProperty("line.separator"))) {
         App.blankLine();
@@ -51,6 +67,12 @@ public class FxGameManager extends GameManager {
     return message;
   }
 
+  /**
+   * A method for displaying the current roster in the currentRosterBox
+   *
+   * @param roster - a string array of player names and info
+   * @param roundNumber - a string representation of the current round
+   */
   public static void displayCurrentRoster(String[] roster, String roundNumber) {
     App.currentRosterBox.getChildren().clear();
     App.blankLine();
@@ -65,17 +87,27 @@ public class FxGameManager extends GameManager {
       TextField textField = new TextField(player.toString());
       textField.setAlignment(Pos.CENTER);
       App.currentRosterBox.getChildren().add(textField);
-
     }
     App.blankLine();
   }
 
+  /**
+   * a method for settint the fight announcement text
+   *
+   * @param p1 - the first player in the fight
+   * @param p2 - the second player in the fight
+   */
   @Override
   public void fightAnnouncement(Player p1, Player p2) {
     String message = String.format("\n----- %s (%1.0f%%) and %s (%1.0f%%) enter the game -----", p1, p1.getBody().getHealth() * 100.0D, p2, p2.getBody().getHealth() * 100.0D);
     App.fightAnnouncement.setText(message);
   }
 
+  /**
+   * a method for displaying the current round announcement
+   *
+   * @param message - a String representation of that round.
+   */
   public String displayRoundAnnouncement(String message) {
     App.roundAnnouncement.setText(message);
     return message;
